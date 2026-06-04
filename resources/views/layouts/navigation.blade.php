804b48c499fa699cbf6cf082d97d3be389eca5b5
fearless-tree <340334@student.mboutrecht.nl>
2026-06-04 13:47:47 +0200

    3.3.11

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -25,6 +25,42 @@
                         </x-nav-link>
                     </div>
                 @endif
+
+                @if (Auth::check() && Auth::user()->rolename === 'tandarts')
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('tandarts.index')"
+                                    :active="request()->routeIs('tandarts.index')">
+                            {{ __('Tandarts') }}
+                        </x-nav-link>
+                    </div>
+                @endif
+
+                @if (Auth::check() && Auth::user()->rolename === 'mondhygienist')
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('mondhygienist.index')"
+                                    :active="request()->routeIs('mondhygienist.index')">
+                            {{ __('Mondhygienist') }}
+                        </x-nav-link>
+                    </div>
+                @endif
+
+                @if (Auth::check() && Auth::user()->rolename === 'assistent')
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('assistent.index')"
+                                    :active="request()->routeIs('assistent.index')">
+                            {{ __('Assistent') }}
+                        </x-nav-link>
+                    </div>
+                @endif
+
+                @if (Auth::check() && Auth::user()->rolename === 'praktijkmanagement')
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                        <x-nav-link :href="route('praktijkmanagement.index')"
+                                    :active="request()->routeIs('praktijkmanagement.index')">
+                            {{ __('Praktijkmanagement') }}
+                        </x-nav-link>
+                    </div>
+                @endif
             </div>
 
             <!-- Settings Dropdown -->
@@ -52,7 +88,7 @@
                             @csrf
 
                             <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
+                                             onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
@@ -63,7 +99,7 @@
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
+                <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                         <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
